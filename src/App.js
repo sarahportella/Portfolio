b6587da7e295322ca76e2dea28c7b66d1d86ab0c
Miguel Silva <migueltjsilva@gmail.com>
2023-10-21 22:02:23 +0000

    modified:   src/App.js 	new file:   src/data/Info.js

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ import 'swiper/css/navigation';
 
 import { EffectCoverflow, Pagination, Navigation , Autoplay} from 'swiper/modules';
 
+import { Info } from './data/Info';
+
 function App() {
   return (
     <div className="container2">
       <section className="frame">
           <div id='title'>
-            <h3>Portfólio 1</h3>
+            <h3>Portfólio</h3>
           </div>
         <p>Conheça os nossos mais novos projetos:</p>
       </section>
@@ -46,80 +48,20 @@ function App() {
         className="swiper_container"
       > 
         
-
-        <SwiperSlide>
-          <div class='slide'id="slide1">
-            <div class='info'>
-              <a href="https://www.behance.net/gallery/156776771/APS-Plus" target='_blank'></a>
-              <h4>APS Plus</h4>
-              <p>Lorem ipsum dolor sit amet. Sed praesentium unde qui architecto unde sed saepe deserunt et modi dolorem.</p>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div class='slide'id="slide2">
-            <div  id='info 2' class='info'>
-              <a href="https://www.behance.net/gallery/173146103/Portfolio-Poda-Facil-Website" target='_blank'></a>
-              <h4>Poda Fácil</h4>
-              <p>Lorem ipsum dolor sit amet. Sed praesentium unde qui architecto unde sed saepe deserunt et modi dolorem.</p>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div class='slide'id="slide3">
-            <div class='info'>
-              <a href="https://www.behance.net/gallery/148971703/Portfolio-Estacao-Povoar" target='_blank'></a>
-            <h4>Estação Povoar</h4>
-            <p>Lorem ipsum dolor sit amet. Sed praesentium unde qui architecto unde sed saepe deserunt et modi dolorem.</p>  
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div class='slide'id="slide4">
-            <div class='info'>
-              <a href="https://www.behance.net/gallery/148526145/Portfolio-Doullyta" target='_blank'></a>
-              <h4>Doullyta</h4>
-              <p>Lorem ipsum dolor sit amet. Sed praesentium unde qui architecto unde sed saepe deserunt et modi dolorem.</p>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div class='slide'id="slide5">
-            <div class='info'>
-              <a href="https://www.behance.net/gallery/148021153/Portfolio-Matheus-Store-(catalogo)" target='_blank'></a>
-              <h4>Catálogo</h4>
-              <p>Lorem ipsum dolor sit amet. Sed praesentium unde qui architecto unde sed saepe deserunt et modi dolorem.</p>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div class='slide'id="slide6">       
-            <div class='info'>
-              <a href="https://www.behance.net/gallery/132534857/Portfolio-Tarefas-Jr" target='_blank'></a>
-              <h4>Tarefas Jr.</h4>
-              <p>Lorem ipsum dolor sit amet. Sed praesentium unde qui architecto unde sed saepe deserunt et modi dolorem.</p>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div class='slide' id="slide7">
-            <div class='info'>
-              <a href="https://www.behance.net/gallery/123493893/Portfolio-App-se" target='_blank'></a>
-              <h4>App-se</h4>
-              <p>Lorem ipsum dolor sit amet. Sed praesentium unde qui architecto unde sed saepe deserunt et modi dolorem.</p>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div class='slide' id="slide8">
-            <div class='info'>
-              <a href="https://www.behance.net/gallery/181122147/Portfolio-Website-Vertuno" target='_blank'></a>
-              <h4>Vertuno</h4>
-              <p>Lorem ipsum dolor sit amet. Sed praesentium unde qui architecto unde sed saepe deserunt et modi dolorem.</p>
-            </div>
-          </div>
-        </SwiperSlide>
-
+        {Info?.map(item => {
+          return(
+            <SwiperSlide>
+              <div class='slide'id={`slide${item.id}`}>
+                <div class='info'>
+                  <a href={`${item.link}`} target='_blank'></a>
+                  <h4>{item.titulo}</h4>
+                  <p>{item.texto}</p>
+                </div>
+              </div>
+            </SwiperSlide>      
+          )
+        })}
+        
         <div className="slider-controler">
           <div className="swiper-button-prev slider-arrow">
             <ion-icon name="arrow-back-circle-outline"></ion-icon>
---
+++ src/data/Info.js
@@ -0,0 +1,50 @@
+export const Info = [
+    {
+        "id": 1,
+        "link": "https://www.behance.net/gallery/156776771/APS-Plus",
+        "titulo": "APS Plus",
+        "texto": "Lorem ipsum dolor sit amet. Sed praesentium unde qui architecto unde sed saepe deserunt et modi dolorem."
+    },
+    {
+        "id": 2,
+        "link": "https://www.behance.net/gallery/173146103/Portfolio-Poda-Facil-Website",
+        "titulo": "Poda Fácil",
+        "texto": "Lorem ipsum dolor sit amet. Sed praesentium unde qui architecto unde sed saepe deserunt et modi dolorem."
+    },
+    {
+        "id": 3,
+        "link": "https://www.behance.net/gallery/148971703/Portfolio-Estacao-Povoar",
+        "titulo": "Estação Povoar",
+        "texto": "Lorem ipsum dolor sit amet. Sed praesentium unde qui architecto unde sed saepe deserunt et modi dolorem."
+    },
+    {
+        "id": 4,
+        "link": "https://www.behance.net/gallery/148526145/Portfolio-Doullyta",
+        "titulo": "Doullyta",
+        "texto": "Lorem ipsum dolor sit amet. Sed praesentium unde qui architecto unde sed saepe deserunt et modi dolorem."
+    },
+    {
+        "id": 5,
+        "link": "https://www.behance.net/gallery/148021153/Portfolio-Matheus-Store-(catalogo)",
+        "titulo": "Catálogo",
+        "texto": "Lorem ipsum dolor sit amet. Sed praesentium unde qui architecto unde sed saepe deserunt et modi dolorem."
+    },
+    {
+        "id": 6,
+        "link": "https://www.behance.net/gallery/132534857/Portfolio-Tarefas-Jr",
+        "titulo": "Tarefas Jr.",
+        "texto": "Lorem ipsum dolor sit amet. Sed praesentium unde qui architecto unde sed saepe deserunt et modi dolorem."
+    },
+    {
+        "id": 7,
+        "link": "https://www.behance.net/gallery/123493893/Portfolio-App-se",
+        "titulo": "App-se",
+        "texto": "Lorem ipsum dolor sit amet. Sed praesentium unde qui architecto unde sed saepe deserunt et modi dolorem."
+    },
+    {
+        "id": 8,
+        "link": "https://www.behance.net/gallery/181122147/Portfolio-Website-Vertuno",
+        "titulo": "Vertuno",
+        "texto": "Lorem ipsum dolor sit amet. Sed praesentium unde qui architecto unde sed saepe deserunt et modi dolorem."
+    },
+]
